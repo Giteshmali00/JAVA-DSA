09f6943f8aa33a1f94c62310e80d2b3b3556cbba
Gitesh <giteshmali430@gmail.com>
2025-06-12 21:34:39 +0530

    Programms to illustrate Loops

---
+++ Loops/SumOfNaturalNumbers.java
@@ -0,0 +1,17 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n : ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum = sum + i;
+        }
+        System.out.print("Sum Of N NAtural Numbers : " + sum);
+    }
+}
