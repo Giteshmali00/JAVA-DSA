b36f89cdfdf7bdb2864e9064cfe5edd6348e264b
Gitesh <giteshmali430@gmail.com>
2025-06-13 21:26:02 +0530

    Programms to demonstrate Methods in Java

---
+++ Methods/SquareOfDigit.java
@@ -0,0 +1,24 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class SquareOfDigit {
+
+    static void squareOfDigit(int n) {
+        int temp = n, count = 0;
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+        System.out.println("Digit = " + count);
+        System.out.println("Square of " + n + " = " + n * n);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number : ");
+        int n = sc.nextInt();
+
+        squareOfDigit(n);
+    }
+}
---
+++ Methods/SquaresOfNNaturalNumbers.java
@@ -0,0 +1,20 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class SquaresOfNNaturalNumbers {
+    static void squares(int n) {
+        System.out.println("Square of Natural Numbers up to " + n);
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + " = " + i * i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a Number n : ");
+        int n = sc.nextInt();
+        squares(n);
+
+    }
+}
