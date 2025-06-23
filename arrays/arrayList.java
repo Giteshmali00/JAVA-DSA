9ca257f10a2a007d955442dad690f0d5d785e16f
Gitesh <giteshmali430@gmail.com>
2025-06-23 22:13:56 +0530

    Added Programms to Demonstrate Arrays

---
+++ arrays/arrayList.java
@@ -0,0 +1,22 @@
+package arrays;
+
+import java.util.ArrayList;
+
+public class arrayList {
+	public static void main(String[] args) {
+		ArrayList<Integer> list = new ArrayList<>();
+
+		list.add(10);
+		list.add(20);
+		list.add(30);
+		list.add(40);
+		System.out.println(list + " " + list.size());
+
+		list.set(1, 200);
+		System.out.println(list + " " + list.size());
+
+		list.remove(1);
+		System.out.print(list + " " + list.size());
+
+	}
+}
