52dfde650f05ca0b7dd0196f87ac8be7a4cece81
Gitesh <191004394+Giteshmali00@users.noreply.github.com>
2025-06-28 22:29:25 +0530

    leetcode: 75.sort colors Dutch Flag Algorithm

---
+++ arrays/sortingDutchFlagAlgorithm.java
@@ -0,0 +1,40 @@
+package arrays;
+
+public class sortingDutchFlagAlgorithm {
+    public static void printArr(int[] arr) {
+        for (int ele : arr) {
+            System.out.print(ele + " ");
+        }
+        System.out.println();
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 0, 2, 1, 0, 2, 1, 0, 0, 2, 1, 1, 2, 2, 2, 1, 1, 1, 0, 0, 0 };
+        int n = arr.length;
+        int low = 0;
+        int mid = 0;
+        int hi = n - 1;
+
+        printArr(arr);
+        while (mid <= hi) {
+            if (arr[mid] == 0) {
+                swap(arr, low, mid);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1)
+                mid++;
+            else if (arr[mid] == 2) {
+                swap(arr, mid, hi--);
+                hi--;
+            } else
+                break;
+        }
+        printArr(arr);
+    }
+}
