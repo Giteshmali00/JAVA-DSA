3c5697562e2a5d58fd4ca042e0714763bab61796
Gitesh <191004394+Giteshmali00@users.noreply.github.com>
2025-07-19 23:49:16 +0530

    Program to Multiplication of Two Matrix

---
+++ JAVA_DSA/two_dimensional_arrays/multiplicationOfMatrix.java
@@ -0,0 +1,42 @@
+package JAVA_DSA.two_dimensional_arrays;
+
+public class multiplicationOfMatrix {
+    public static void print2DArray(int[][] arr) {
+        for (int[] ele : arr) {
+            for (int sele : ele) {
+                System.out.print(sele + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[][] a = {
+                { 1, 2, 3 },
+                { 5, 6, 7 },
+                { 9, 2, 5 },
+                { 9, 2, 5 }
+        };
+        int[][] b = {
+                { 5, 8, 9 },
+                { 6, 9, 3 },
+                { 3, 8, 9 }
+        };
+
+        if (a[0].length != b.length) {
+            System.out.print("Multiplication Not Possible..!");
+        } else {
+            int[][] ans = new int[a.length][b[0].length];
+            for (int i = 0; i < ans.length; i++) {
+                for (int j = 0; j < ans[0].length; j++) {
+                    for (int k = 0; k < b.length; k++) {
+                        ans[i][j] += a[i][k] * b[k][j];
+                    }
+                }
+
+            }
+            print2DArray(ans);
+        }
+    }
+}
