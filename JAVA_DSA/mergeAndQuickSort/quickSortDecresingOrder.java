e34393b0696f6f661b34f79a48f5a5f471081f2e
Gitesh <191004394+Giteshmali00@users.noreply.github.com>
2025-12-10 10:27:33 +0530

    Practice Questions of Modul 21

---
+++ JAVA_DSA/mergeAndQuickSort/quickSortDecresingOrder.java
@@ -0,0 +1,59 @@
+package JAVA_DSA.mergeAndQuickSort;
+
+public class quickSortDecresingOrder {
+    public static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static int partition(int[] arr, int lo, int hi) {
+        int mid = (lo + hi) / 2;
+        int pivot = arr[mid];
+        int smc = 0;
+        for (int i = lo; i <= hi; i++) {
+            if (i == mid)
+                continue;
+            if (arr[i] < pivot)
+                smc++;
+        }
+        int cidx = hi - smc;
+        swap(arr, mid, cidx);
+        int i = lo, j = hi;
+        while (i < cidx && j > cidx) {
+            if (arr[i] >= pivot)
+                i++;
+            else if (arr[j] < pivot)
+                j--;
+            else if (arr[i] < pivot && arr[j] >= pivot) {
+                swap(arr, i, j);
+                i++;
+                j--;
+            }
+        }
+
+        return cidx;
+    }
+
+    public static void quickSort(int[] arr, int lo, int hi) {
+        if (lo >= hi)
+            return;
+        int indx = partition(arr, lo, hi);
+        quickSort(arr, lo, indx - 1);
+        quickSort(arr, indx + 1, hi);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 4, 5, 2, 6, 1, 7 };
+        print(arr);
+        quickSort(arr, 0, arr.length - 1);
+        print(arr);
+    }
+}
---
+++ JAVA_DSA/mergeAndQuickSort/reversePairs.java
@@ -0,0 +1,71 @@
+package JAVA_DSA.mergeAndQuickSort;
+
+public class reversePairs {
+    static int count;
+
+    public static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void merge(int[] a, int[] b, int[] c) {
+        int i = 0, j = 0, k = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] <= b[j])
+                c[k++] = a[i++];
+            else
+                c[k++] = b[j++];
+        }
+        while (i < a.length)
+            c[k++] = a[i++];
+        while (j < b.length)
+            c[k++] = b[j++];
+    }
+
+    public static void inversion(int[] a, int[] b) {
+        int i = 0, j = 0;
+        while (i < a.length && j < b.length) {
+            if (a[i] > 2 * (long) b[j]) {
+                count += (a.length - i);
+                j++;
+            } else
+                i++;
+        }
+    }
+
+    public static void mergeSelect(int[] arr) {
+        int n = arr.length;
+        if (n <= 1)
+            return;
+        int[] a = new int[n / 2];
+        int[] b = new int[n - n / 2];
+
+        for (int i = 0; i < n / 2; i++) {
+            a[i] = arr[i];
+        }
+        for (int i = 0; i < n - n / 2; i++) {
+            b[i] = arr[i + n / 2];
+        }
+
+        mergeSelect(a);
+        mergeSelect(b);
+
+        inversion(a, b);
+
+        merge(a, b, arr);
+        a = null;
+        b = null;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 6, 8, 3, 9, 1, 4 };
+        count = 0;
+        print(arr);
+        mergeSelect(arr);
+        print(arr);
+        System.out.println(count);
+    }
+}
