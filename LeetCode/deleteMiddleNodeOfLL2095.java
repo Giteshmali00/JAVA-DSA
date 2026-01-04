930c94567374ed59d83cf90e587b1009597c70cf
Gitesh <191004394+Giteshmali00@users.noreply.github.com>
2026-01-04 09:52:07 +0530

    Linked List LeetCode

---
+++ LeetCode/deleteMiddleNodeOfLL2095.java
@@ -0,0 +1,56 @@
+package LeetCode;
+
+public class deleteMiddleNodeOfLL2095 {
+    // Method 1 :-
+    public static Node deleteMiddle(Node head) {
+        Node temp = head;
+        int n = 0;// size of the Linked List
+        while (temp != null) {
+            temp = temp.next;
+            n++;
+        }
+        if (n == 1)
+            return null;
+        temp = head;
+        for (int i = 0; i < n / 2 - 1; i++) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+
+        return head;
+
+    }
+
+    // Method 2 :- Slightly better than Method 1
+    public static Node deleteMiddle2(Node head) {
+        if (head.next == null)
+            return null;// edge case
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null && fast.next.next != null && fast.next.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+
+        return head;
+
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertAtTail(1);
+        list.insertAtTail(2);
+        list.insertAtTail(4);
+        list.insertAtTail(3);
+        list.insertAtTail(8);
+        list.insertAtTail(6);
+
+        list.display();
+        list.head = deleteMiddle(list.head);
+        list.display();
+
+        list.head = deleteMiddle2(list.head);
+        list.display();
+    }
+}
---
+++ LeetCode/swappingNodesInLL1721.java
@@ -0,0 +1,111 @@
+package LeetCode;
+
+public class swappingNodesInLL1721 {
+    // Method 1 :- TC = O(~3n) which means O(n) Not good...
+    public static Node swapNodes(Node head, int k) {
+        Node temp = head;
+        int n = 0;
+        while (temp != null) {
+            temp = temp.next;
+            n++;
+        }
+        Node n1 = head;
+        Node n2 = head;
+
+        for (int i = 1; i < k; i++) {
+            n1 = n1.next;
+        }
+        for (int i = 1; i <= n - k; i++) {
+            n2 = n2.next;
+        }
+
+        int t = n1.val;
+        n1.val = n2.val;
+        n2.val = t;
+
+        return head;
+    }
+
+    // Method 2 :- Slightly Better than Method 1
+    public static Node swapNodes2(Node head, int k) {
+        Node temp = head;
+        int n = 0;
+        while (temp != null) {
+            temp = temp.next;
+            n++;
+        }
+        Node n1 = head;
+        Node n2 = head;
+
+        for (int i = 1; i < k; i++) {
+            n1 = n1.next;
+        }
+        int pnk = n - k;
+        if (pnk < 0) {
+            pnk = -pnk;
+        }
+        for (int i = 1; i <= pnk; i++) {
+            n2 = n2.next;
+        }
+
+        int t = n1.val;
+        n1.val = n2.val;
+        n2.val = t;
+
+        return head;
+    }
+
+    // Method 3 :- Slight Better than Methd 1 & 2
+    public static Node swapNodes3(Node head, int k) {
+        Node temp = head;
+        int n = 0;
+        while (temp != null) {
+            temp = temp.next;
+            n++;
+        }
+        Node n1 = head;
+        Node n2 = head;
+
+        for (int i = 1; i < k; i++) {
+            n1 = n1.next;
+        }
+        int pnk = n - k;
+        if (pnk <= 0 || k >= n / 2) {
+            if (pnk < 0)
+                pnk = -pnk;
+            for (int i = 1; i <= pnk; i++) {
+                n2 = n2.next;
+            }
+        } else {
+            n2 = n1;
+            for (int i = 1; i <= pnk - k + 1; i++) {
+                n2 = n2.next;
+            }
+        }
+
+        int t = n1.val;
+        n1.val = n2.val;
+        n2.val = t;
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertAtTail(1);
+        list.insertAtTail(2);
+        list.insertAtTail(3);
+        list.insertAtTail(4);
+        list.insertAtTail(5);
+
+        list.display();
+        list.head = swapNodes(list.head, 2);
+        list.display();
+
+        list.head = swapNodes2(list.head, 1);
+        list.display();
+
+        list.head = swapNodes3(list.head, 4);
+        list.display();
+    }
+}
