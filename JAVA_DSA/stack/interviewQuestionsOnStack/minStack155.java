f3ecc9c1d9ea60e2eeda2440366f91c8f345e271
Gitesh <191004394+Giteshmali00@users.noreply.github.com>
2026-02-22 22:47:23 +0530

    Stack leetCode 155

--- JAVA_DSA/stack/interviewQuestionsOnStack/minStack155.java
+++ JAVA_DSA/stack/interviewQuestionsOnStack/minStack155.java
@@ -1,12 +1,53 @@
-package interviewQuestionsOnStack;
+//package interviewQuestionsOnStack;
+import java.util.Stack;
 
-public class minStack {
-    public static class Stack{
-        public void push(int val){
-            Stac
+public class minStack155 {
+    public static class MinStack{
+        Stack<Long> st = new Stack<>();
+        long min = 0;
+        void push(int val) {
+            long x = (long)val;
+            if(st.size() == 0){
+                min = x;
+                st.push(x);
+            }else if(x < min){
+                st.push(2*x-min);
+                min = x;
+            }
+            else{
+                st.push(x);
+            }
+        }
+
+        void pop() {
+            if(st.peek() < min)
+                min = 2*min - st.pop();
+            else
+                st.pop();
+        }
+
+        int top() {
+            if(st.peek() < min)
+                return (int)min;
+            else{
+                long x = st.peek();
+                return (int)x;
+            }
+        }
+
+        int getMin() {
+            return (int)min;
         }
     }
     public static void main(String[] args){
+        MinStack ms = new MinStack();
+        ms.push(4);
+        ms.push(2);
+        ms.push(7);
+        ms.push(1);
+        System.out.println("Minimum Element of current Stack is : "+ms.getMin());
+        ms.pop();
+        System.out.println("Minimum Element of current Stack is : "+ms.getMin());
 
     }
 }
