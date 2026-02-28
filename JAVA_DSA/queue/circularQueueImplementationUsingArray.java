e66a1d7cb9d7fc0f7f1e4979673e4366d64f23eb
Gitesh <191004394+Giteshmali00@users.noreply.github.com>
2026-02-28 09:39:01 +0530

    Circular Queue Implementation using Array

--- JAVA_DSA/queue/circularQueueImplementationUsingArray.java
+++ JAVA_DSA/queue/circularQueueImplementationUsingArray.java
@@ -3,30 +3,48 @@ public class circularQueueImplementationUsingArray {
         private int[] arr = new int[5];
         private int n = arr.length;
         private int size = 0;
-        private int front = 0;
-        private int rear = 0;
+        private int front = -1;
+        private int rear = -1;
         void add(int val)throws Error{
             if(size==n) throw new Error("Queue is Full!");
-            else if(rear==n-1) {
-                arr[rear] = val;
+            else if(size == 0){
+                front = rear = 0;
+                arr[0] = val;
+            }else if(rear < n-1){
+                arr[++rear] = val;
+            }
+            else if(rear == n-1){
                 rear = 0;
-            }else arr[rear++] = val;
+                arr[0] = val;
+            }
+
             size++;
         }
         int poll() throws Error{
             if(size==0) throw new Error("Queue is Empty!");
             int x = arr[front];
-            if(front==n-1) front = 0;
+            if(front == n-1) front = 0;
             else front++;
             size--;
             return x;
         }
+        int peek()throws Error{
+            if(size==0) throw new Error("Queue is Empty!");
+            return arr[front];
+        }
         void display(){
-            if(size==0) System.out.println("Queue is Empty!");
-            for(int i = front; i < size; i++){
-                System.out.print(arr[i]+" ");
+            if(size==0){
+                System.out.println("Queue is Empty!");
+                return;
             }
-            System.out.println();
+            int i = front;
+            while(i != rear){
+                System.out.print(arr[i]+" ");
+                if(i == n-1) i = 0;
+                else i++;
+            }
+            System.out.println(arr[i]+" ");
+
         }
 
         int size(){return size;}
@@ -37,15 +55,20 @@ public class circularQueueImplementationUsingArray {
         q.add(2);
         q.add(4);
         q.add(6);
+        q.add(7);
         q.display();
         q.poll();
         q.poll();
         q.poll();
         q.poll();
+//        q.poll();
         q.add(10);
         q.add(20);
-        q.display();
+        q.add(30);
+        q.add(40);
+//        q.add(50);
 
+        q.display();
         System.out.println("Size : "+q.size());
     }
 }
