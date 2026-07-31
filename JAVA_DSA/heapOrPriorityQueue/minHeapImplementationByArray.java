812f002ae065b74b76eb69494463251585bf38ac
Gitesh <191004394+Giteshmali00@users.noreply.github.com>
2026-07-31 20:42:09 +0530

    Heap Implementation using array

--- JAVA_DSA/heapOrPriorityQueue/minHeapImplementationByArray.java
+++ JAVA_DSA/heapOrPriorityQueue/minHeapImplementationByArray.java
@@ -1,3 +1,81 @@
+class MinHeap{
+    private int[] heap;
+    private int size;
+    MinHeap(int cap){
+        heap = new int[cap];
+        size = 0;
+    }
+    void add(int val) throws Exception{
+        if(size==heap.length) throw new Exception("Heap is Full!");
+        heap[size++] = val;
+        upheapify(size-1);
+    }
+    void upheapify(int idx){
+        if(idx==0) return;
+        int parent = (idx-1)/2;
+        if(heap[parent] > heap[idx]){
+            swap(parent,idx);
+            upheapify(parent);
+        }
+    }
+    void swap(int x, int y){
+        int temp = heap[x];
+        heap[x] = heap[y];
+        heap[y] = temp;
+    }
+    int peek() throws Exception{
+        if(size==0) throw new Error("Heap is Empty!");
+        return heap[0];
+    }
+    int size(){
+        return size;
+    }
+    int remove() throws Exception{
+        if(size==0) throw new Exception("Heap is Empty!");
+        int x = heap[0];
+        heap[0] = heap[size-1];
+        size--;
+        downheapify(0);
+        return x;
+    }
+    void downheapify(int parent){
+        int lc = parent*2+1, rc = parent*2+2;
+        int mindx = parent;
+        if(lc < size && heap[lc] < heap[mindx]) mindx = lc;
+        else if(rc < size && heap[rc] < heap[mindx]) mindx = rc;
+        else return;
+        swap(mindx,parent);
+        downheapify(mindx);
+    }
+}
 public class minHeapImplementationByArray {
+    static void main(String[] args) throws Exception{
+        MinHeap heap = new MinHeap(10);
+        heap.add(10);
+        heap.add(30);
+        heap.add(40);
+//        heap.add(25);
+        System.out.println("Size : "+heap.size());
+//        heap.add(20);
+//        heap.add(20);
+//        heap.add(15);
+//        heap.add(40);
+//        heap.add(35);
+//        heap.add(35);
+//        heap.add(35);
+//        heap.add(35);
+//        heap.remove();
+//        heap.add(3);
+//        heap.remove();
+//        heap.remove();
+//        heap.remove();
+//        heap.add(27);
 
+//        System.out.println(heap.remove());
+        while(heap.size()>0){
+            System.out.print(heap.remove()+" ");
+        }
+        System.out.println();
+        System.out.println("Size : "+heap.size());
+    }
 }
--- LeetCode/kthLargestElementInAStream703.java
+++ LeetCode/kthLargestElementInAStream703.java
@@ -1,3 +1,32 @@
-public class kthLargestElementInAStream703 {
+import java.util.PriorityQueue;
 
+public class kthLargestElementInAStream703 {
+    public static class KthLargest {
+        PriorityQueue<Integer> pq;
+        int k;
+        public KthLargest(int k, int[] nums) {
+            pq = new PriorityQueue<>();
+            for(int i = 0; i < nums.length; i++){
+                pq.add(nums[i]);
+                if(pq.size()>k) pq.remove();
+            }
+            this.k = k;
+        }
+
+        public int add(int val) {
+            pq.add(val);
+            if(pq.size()>k) pq.remove();
+            return pq.peek();
+        }
+    }
+
+    static void main(String[] args) {
+        int[] nums = {4,5,8,2};
+        KthLargest stream = new KthLargest(3,nums);
+        System.out.println(stream.add(3));
+        System.out.println(stream.add(5));
+        System.out.println(stream.add(10));
+        System.out.println(stream.add(9));
+        System.out.println(stream.add(4));
+    }
 }
